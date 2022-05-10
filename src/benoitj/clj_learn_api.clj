7ac6b7f95d730d8cb0cffbf18522edbd0456dfab
Benoit Joly <benoit@benoitj.ca>
2022-05-10 08:55:55 -0400

    basic user management functions
    not yet integrated in the api

--- src/benoitj/clj_learn_api.clj
+++ src/benoitj/clj_learn_api.clj
@@ -9,8 +9,29 @@
 
 (defonce server (atom nil))
 
+
+(def users (atom {1 {:id 1 :name "John"}
+                  2 {:id 2 :name "Jane"}}))
+
+(defn get-users []
+  (into [] (map second @users)))
+
+(defn get-user [id]
+  (get @users id))
+
+(defn add-user [user]
+  (when (not (@users (:id user)))
+    (swap! users #(assoc % (:id user) user))))
+
+
+
+(defn del-user [id]
+  (swap! users #(dissoc % id)))
+
+
 (defroutes routes
   (GET "/" [] {:body {:response "OK"}})
+  (GET "/users" [] {:body (get-users)})
   (route/not-found "Page not found"))
 
 (defn start-jetty! [& _]
@@ -27,7 +48,14 @@
 (defn -main [& _]
   (reset! server (start-jetty!)))
 
-
 (comment
   (-main)
-  (stop-jetty!))
+  (stop-jetty!)
+  (add-user {:id 7 :name "foobar"})
+  (add-user {:id 7 :name "not working"})
+  (add-user {:id 4 :name "foobaz"})
+  (del-user 1)
+  (get-user 2)
+  (get-users)
+  @users
+  )
